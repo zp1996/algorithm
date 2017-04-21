3a22c486430c004379dc28ad68e22c047c504076
周鹏 <1179258516@qq.com>
2017-04-21 20:14:46 +0800

    add QuickSort

--- src/Algorithm/Sort/QuickSort.go
+++ src/Algorithm/Sort/QuickSort.go
@@ -1,8 +1,31 @@
 package Algorithm
 
-import "sort"
+func qSort(arr []int, left, right int) {
+	current := arr[left]
+	i := left
+	j := right
+	for i < j {
+		for arr[j] > current && i < j {
+			j--
+		}
+		for arr[i] <= current && i < j {
+			i++
+		}
+		if i < j {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[left] = arr[i]
+	arr[i] = current
+	if left < j-1 {
+		qSort(arr, left, j-1)
+	}
+	if right > j+1 {
+		qSort(arr, j+1, right)
+	}
+}
 
 func QucikSort(arr []int) []int {
-	sort.Ints(arr)
+	qSort(arr, 0, len(arr)-1)
 	return arr
 }
